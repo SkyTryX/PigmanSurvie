d4d6bc57681d17c1a97a58ac34d511b1775f3c1f
SkyTryX <dorian.gil34@gmail.com>
2023-07-12 00:52:54 +0200

    first commit

---
+++ src/main/java/fr/skytryx/pigmansurvie/PigmanSurvie.java
@@ -0,0 +1,17 @@
+package fr.skytryx.pigmansurvie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PigmanSurvie extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
