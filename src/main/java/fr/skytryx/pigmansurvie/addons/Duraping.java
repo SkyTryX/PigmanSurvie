fc457dbcdb84295be7161b798a92350997e03d5e
Matteo Melandri <80131198+Matteo34800-cpu@users.noreply.github.com>
2023-07-13 17:33:18 +0200

    tient mon gaté ajout duraping

--- src/main/java/fr/skytryx/pigmansurvie/addons/Duraping.java
+++ src/main/java/fr/skytryx/pigmansurvie/addons/Duraping.java
@@ -1,7 +1,6 @@
 package fr.skytryx.pigmansurvie.addons;
 
 import org.bukkit.Sound;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
@@ -12,7 +11,7 @@ public class Duraping implements Listener {
     @EventHandler
     public void ping(PlayerItemDamageEvent event){
         Damageable im = (Damageable) event.getItem().getItemMeta();
-        if(event.getItem().getType().getMaxDurability() - im.getDamage() == 10){
+        if( (double) im.getDamage() / event.getItem().getType().getMaxDurability()  == 0.1){
             event.getPlayer().sendMessage("§c[Outils] §4ATTENTION! §bTon outils va bientôt §6casser");
             event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_ANVIL_BREAK,50.0f,1.0f);
         }
