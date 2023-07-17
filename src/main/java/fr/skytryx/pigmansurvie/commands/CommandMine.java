96d7b53387b78826f9dfb8492610d0302654f18a
Matte <melandridalloyau.matteo@gmail.com>
2023-07-17 18:46:14 +0200

    implementation de mineworld

--- src/main/java/fr/skytryx/pigmansurvie/commands/CommandMine.java
+++ src/main/java/fr/skytryx/pigmansurvie/commands/CommandMine.java
@@ -15,7 +15,16 @@ public class CommandMine implements CommandExecutor {
     //Verification de l'auteur de la commande
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if(!(commandSender instanceof Player)) return false;
-        ((Player)commandSender).teleport(Objects.requireNonNull(Bukkit.getWorld("mineworld")).getSpawnLocation());
+        Player player = (Player) commandSender;
+        if (player.getWorld() == Bukkit.getWorld("mineworld")) {
+            player.teleport(Objects.requireNonNull(Bukkit.getWorld("world")).getSpawnLocation());
+            player.sendMessage("Te voila de retour chez toi !!!");
+
+        }
+        else{
+            player.teleport(Objects.requireNonNull(Bukkit.getWorld("mineworld")).getSpawnLocation());
+            player.sendMessage("Te voila en minage !!!");
+        }
         return true;
     }
 }
