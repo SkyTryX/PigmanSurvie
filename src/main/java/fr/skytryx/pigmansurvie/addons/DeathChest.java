ab667631576135761d75d4187d7501ce5b0a39a3
SkyTryX <dorian.gil34@gmail.com>
2023-07-17 18:05:01 +0200

    petit changement

--- src/main/java/fr/skytryx/pigmansurvie/addons/DeathChest.java
+++ src/main/java/fr/skytryx/pigmansurvie/addons/DeathChest.java
@@ -1,20 +1,13 @@
 package fr.skytryx.pigmansurvie.addons;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.block.Chest;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 public class DeathChest implements Listener {
@@ -23,42 +16,20 @@ public class DeathChest implements Listener {
     public void DeathChestSpawn(PlayerDeathEvent event){
         Location deathloc = event.getPlayer().getLocation();
         event.getPlayer().getWorld().getBlockAt(deathloc).setType(Material.CHEST);
-        final File file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager()
-                .getPlugin("PigmanSurvie")).getDataFolder(), "deathchest.yml");
-        final YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        BlockData block = event.getPlayer().getWorld().getBlockAt(deathloc).getBlockData();
 
-        List<ItemStack> player_item = new ArrayList<>();
         for(int i = 0; i < 36; i++){
-            player_item.add(event.getPlayer().getInventory().getItem(i));
+            ((Chest)block).getBlockInventory().addItem(Objects.requireNonNull(event.getPlayer().getInventory().getItem(i)));
+            if(Objects.requireNonNull(((Chest)block).getBlockInventory().getItem(((Chest)block).getBlockInventory().getSize())).getType() != Material.AIR){
+                Location newchest = new Location(deathloc.getWorld(), deathloc.getBlockX()+1, deathloc.getBlockY(), deathloc.getBlockZ());
+                newchest.getBlock().setType(Material.CHEST);
+                block = newchest.getBlock().getBlockData();
+            }
         }
-        player_item.add(event.getPlayer().getInventory().getHelmet());
-        player_item.add(event.getPlayer().getInventory().getChestplate());
-        player_item.add(event.getPlayer().getInventory().getLeggings());
-        player_item.add(event.getPlayer().getInventory().getBoots());
-        player_item.add(event.getPlayer().getInventory().getItemInOffHand());
-        config.set(String.valueOf(deathloc), player_item.toString());
-        try {
-            config.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @EventHandler
-    public void OnDeathChestOpen(PlayerInteractEvent event){
-        //if(Objects.requireNonNull(event.getClickedBlock()).getType() != Material.CHEST) return;
-        //final File file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager()
-        //        .getPlugin("PigmanSurvie")).getDataFolder(), "deathchest.yml");
-        //final YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
-        //if(config.contains(event.getClickedBlock().getLocation().toString())){
-            //String list_string= (String) config.get(event.getClickedBlock().getLocation().toString());
-            //assert list_string != null;
-            //list_string = list_string.replace("[","");
-            //list_string= list_string.replace("]","");
-            //List<String> player_inv = new ArrayList<>(Arrays.asList(list_string.split(",")));
-            //for(int i = 0; i < 36; i++){
-                //event.getPlayer().getInventory().setItem(i, (ItemStack) player_inv.get(i));
-            //}
-        //}
+        ((Chest)block).getBlockInventory().addItem(Objects.requireNonNull(event.getPlayer().getInventory().getHelmet()));
+        ((Chest)block).getBlockInventory().addItem(Objects.requireNonNull(event.getPlayer().getInventory().getChestplate()));
+        ((Chest)block).getBlockInventory().addItem(Objects.requireNonNull(event.getPlayer().getInventory().getLeggings()));
+        ((Chest)block).getBlockInventory().addItem(Objects.requireNonNull(event.getPlayer().getInventory().getBoots()));
+        ((Chest)block).getBlockInventory().addItem(event.getPlayer().getInventory().getItemInOffHand());
     }
 }
