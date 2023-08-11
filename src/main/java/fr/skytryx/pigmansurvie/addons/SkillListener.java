b0d104c8c8a3e4db0337add1c69e4632340c47ad
SkyTryX <dorian.gil34@gmail.com>
2023-08-11 19:38:03 +0200

    - beta version of skills - fixed a big bug on the SkillListener.java

--- src/main/java/fr/skytryx/pigmansurvie/addons/SkillListener.java
+++ src/main/java/fr/skytryx/pigmansurvie/addons/SkillListener.java
@@ -1,8 +1,11 @@
 package fr.skytryx.pigmansurvie.addons;
 
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -19,6 +22,12 @@ import java.util.Objects;
 
 public class SkillListener implements Listener {
 
+    public void GetXP(String skill, YamlConfiguration config, Player player, Float xp){
+        config.set(player.getUniqueId() + "."+skill+".xp", config.getInt(player.getUniqueId() + "."+skill+".xp") + xp);
+        BossBar xp_bar = BossBar.bossBar(Component.text("Gained "+ xp + " XP"), 0f, BossBar.Color.GREEN, BossBar.Overlay.PROGRESS);
+        player.showBossBar(xp_bar);
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("PigmanSurvie")), () -> player.hideBossBar(xp_bar), 40L);
+    }
     @EventHandler
     public void ProfileCreation(PlayerJoinEvent event){
         final File skillfile = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("PigmanSurvie")).getDataFolder(), "skills.yml");
@@ -37,7 +46,7 @@ public class SkillListener implements Listener {
     }
 
 
-    Map<Material, Float> Miner  = new HashMap<Material, Float>() {{
+    Map<Material, Float> Miner = new HashMap<Material, Float>() {{
         put(Material.STONE , 1f);
         put(Material.COAL_ORE, 5f);
         put(Material.DEEPSLATE, 1.5f);
@@ -48,6 +57,44 @@ public class SkillListener implements Listener {
         put(Material.DEEPSLATE_GOLD_ORE, 17.5f);
         put(Material.DIAMOND_ORE, 30f);
         put(Material.DEEPSLATE_DIAMOND_ORE, 35f);
+        put(Material.REDSTONE_ORE, 10f);
+        put(Material.DEEPSLATE_REDSTONE_ORE, 12f);
+        put(Material.LAPIS_ORE, 20f);
+        put(Material.DEEPSLATE_LAPIS_ORE, 25f);
+        put(Material.ANCIENT_DEBRIS, 300f);
+        put(Material.COPPER_ORE, 8f);
+        put(Material.DEEPSLATE_COPPER_ORE, 9f);
+        put(Material.NETHER_QUARTZ_ORE, 10f);
+        put(Material.NETHER_GOLD_ORE, 7f);
+        put(Material.EMERALD_ORE, 500f);
+        put(Material.DEEPSLATE_EMERALD_ORE, 700f);
+    }};
+    Map<Material, Float> Farmer = new HashMap<Material, Float>() {{
+        put(Material.POTATOES, 10f);
+        put(Material.CARROTS, 10f);
+        put(Material.BEETROOTS, 15f);
+        put(Material.SUGAR_CANE, 10f);
+    }};
+    Map<Material, Float> WoodCutter = new HashMap<Material, Float>() {{
+       put(Material.ACACIA_LOG, 10f);
+       put(Material.BIRCH_LOG, 10f);
+       put(Material.CHERRY_LOG, 10f);
+       put(Material.JUNGLE_LOG, 10f);
+       put(Material.DARK_OAK_LOG, 10f);
+       put(Material.MANGROVE_LOG, 10f);
+       put(Material.SPRUCE_LOG, 10f);
+       put(Material.OAK_LOG, 10f);
+       put(Material.AZALEA, 20f);
+    }};
+    Map<Material, Float> Excavater = new HashMap<Material, Float>() {{
+        put(Material.DIRT, 1f);
+        put(Material.GRASS_BLOCK, 2f);
+        put(Material.MYCELIUM, 10f);
+        put(Material.PODZOL, 8f);
+        put(Material.GRAVEL, 3f);
+        put(Material.SAND, 3f);
+        put(Material.SUSPICIOUS_GRAVEL, 100f);
+        put(Material.SUSPICIOUS_SAND, 100f);
     }};
     @EventHandler
     public void XPGain(BlockBreakEvent event){
@@ -55,7 +102,13 @@ public class SkillListener implements Listener {
         final YamlConfiguration skillconfig = YamlConfiguration.loadConfiguration(skillfile);
         if(event.getBlock().getMetadata("PLACED").isEmpty()) {
             if (Miner.containsKey(event.getBlock().getType())) {
-                skillconfig.set(event.getPlayer().getUniqueId() + ".mining.xp", skillconfig.getInt(event.getPlayer().getUniqueId() + ".mining.xp") + Miner.get(event.getBlock().getType()));
+                GetXP("mining", skillconfig, event.getPlayer(), Miner.get(event.getBlock().getType()));
+            } else if (Farmer.containsKey(event.getBlock().getType())) {
+                GetXP("farming", skillconfig, event.getPlayer(), Farmer.get(event.getBlock().getType()));
+            } else if (WoodCutter.containsKey(event.getBlock().getType())) {
+                GetXP("woodcutting", skillconfig, event.getPlayer(), WoodCutter.get(event.getBlock().getType()));
+            } else if (Excavater.containsKey(event.getBlock().getType())){
+                GetXP("excavating", skillconfig, event.getPlayer(), Excavater.get(event.getBlock().getType()));
             }
         }
         try {
