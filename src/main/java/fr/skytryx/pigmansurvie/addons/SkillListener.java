8ed8f1f1c7547c7f37f4d56445a57ee22d08d343
SkyTryX <dorian.gil34@gmail.com>
2023-08-11 10:49:58 +0200

    - beta version of skills

--- src/main/java/fr/skytryx/pigmansurvie/addons/SkillListener.java
+++ src/main/java/fr/skytryx/pigmansurvie/addons/SkillListener.java
@@ -1,14 +1,20 @@
 package fr.skytryx.pigmansurvie.addons;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SkillListener implements Listener {
@@ -31,4 +37,36 @@ public class SkillListener implements Listener {
     }
 
 
+    Map<Material, Float> Miner  = new HashMap<Material, Float>() {{
+        put(Material.STONE , 1f);
+        put(Material.COAL_ORE, 5f);
+        put(Material.DEEPSLATE, 1.5f);
+        put(Material.DEEPSLATE_COAL_ORE, 6f);
+        put(Material.IRON_ORE, 9f);
+        put(Material.DEEPSLATE_IRON_ORE, 10f);
+        put(Material.GOLD_ORE, 15f);
+        put(Material.DEEPSLATE_GOLD_ORE, 17.5f);
+        put(Material.DIAMOND_ORE, 30f);
+        put(Material.DEEPSLATE_DIAMOND_ORE, 35f);
+    }};
+    @EventHandler
+    public void XPGain(BlockBreakEvent event){
+        final File skillfile = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("PigmanSurvie")).getDataFolder(), "skills.yml");
+        final YamlConfiguration skillconfig = YamlConfiguration.loadConfiguration(skillfile);
+        if(event.getBlock().getMetadata("PLACED").isEmpty()) {
+            if (Miner.containsKey(event.getBlock().getType())) {
+                skillconfig.set(event.getPlayer().getUniqueId() + ".mining.xp", skillconfig.getInt(event.getPlayer().getUniqueId() + ".mining.xp") + Miner.get(event.getBlock().getType()));
+            }
+        }
+        try {
+            skillconfig.save(skillfile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @EventHandler
+    public void AntiDupe(BlockPlaceEvent event){
+        event.getBlock().setMetadata("PLACED", new FixedMetadataValue(Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("PigmanSurvie")), "PLACED"));
+    }
 }
