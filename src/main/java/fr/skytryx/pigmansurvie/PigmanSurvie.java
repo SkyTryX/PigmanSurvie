3c26d8caa5ed6a926ecae8b241abe2d22aac1107
SkyTryX <dorian.gil34@gmail.com>
2023-10-10 20:19:12 +0200

    - removed recipes

--- src/main/java/fr/skytryx/pigmansurvie/PigmanSurvie.java
+++ src/main/java/fr/skytryx/pigmansurvie/PigmanSurvie.java
@@ -6,9 +6,7 @@ import fr.skytryx.pigmansurvie.staff.InvseeCheck;
 import fr.skytryx.pigmansurvie.staff.XrayAlerts;
 import fr.skytryx.pigmansurvie.staff.VillagerAlerts;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.WorldCreator;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
@@ -27,13 +25,13 @@ public final class PigmanSurvie extends JavaPlugin {
         if(Bukkit.getWorld("mineworld") == null){
             new WorldCreator("mineworld").createWorld();
         }
-        Util.CreateRecipe(Util.CreateHead("Coal", CommandLuckyBlock.LB_Texture.get("Coal")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.COAL_BLOCK);}}, "Coal_LB");
-        Util.CreateRecipe(Util.CreateHead("Iron", CommandLuckyBlock.LB_Texture.get("Iron")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.IRON_BLOCK);}}, "Iron_LB");
-        Util.CreateRecipe(Util.CreateHead("Gold", CommandLuckyBlock.LB_Texture.get("Gold")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.GOLD_BLOCK);}}, "Gold_LB");
-        Util.CreateRecipe(Util.CreateHead("Diamond", CommandLuckyBlock.LB_Texture.get("Diamond")), Arrays.asList("   ", "C C", "   "), new HashMap<Character, Material>(){{put('C', Material.DIAMOND_BLOCK);}}, "Diamond_LB");
-        Util.CreateRecipe(Util.CreateHead("Netherite", CommandLuckyBlock.LB_Texture.get("Netherite")), Arrays.asList("C C", "C C", "C C"), new HashMap<Character, Material>(){{put('C', Material.NETHERITE_INGOT);}}, "Netherite_LB");
-        Util.CreateRecipe(Util.CreateHead("Redstone", CommandLuckyBlock.LB_Texture.get("Redstone")), Arrays.asList("SSS", "SCS", "SSS"), new HashMap<Character, Material>(){{put('C', Material.REDSTONE_BLOCK); put('S', Material.SCULK);}}, "Redstone_LB");
-        Util.CreateRecipe(Util.CreateHead("Emerald", CommandLuckyBlock.LB_Texture.get("Emerald")), Arrays.asList("LBL", "EDE", "LBL"), new HashMap<Character, Material>(){{put('L', Material.LAPIS_LAZULI); put('B', Material.LAPIS_BLOCK); put('E', Material.EMERALD_BLOCK); put('D', Material.DIAMOND);}}, "Emerald_LB");
+        //Util.CreateRecipe(Util.CreateHead("Coal", CommandLuckyBlock.LB_Texture.get("Coal")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.COAL_BLOCK);}}, "Coal_LB");
+        //Util.CreateRecipe(Util.CreateHead("Iron", CommandLuckyBlock.LB_Texture.get("Iron")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.IRON_BLOCK);}}, "Iron_LB");
+        //Util.CreateRecipe(Util.CreateHead("Gold", CommandLuckyBlock.LB_Texture.get("Gold")), Arrays.asList(" C ", "C C", " C "), new HashMap<Character, Material>(){{put('C', Material.GOLD_BLOCK);}}, "Gold_LB");
+        //Util.CreateRecipe(Util.CreateHead("Diamond", CommandLuckyBlock.LB_Texture.get("Diamond")), Arrays.asList("   ", "C C", "   "), new HashMap<Character, Material>(){{put('C', Material.DIAMOND_BLOCK);}}, "Diamond_LB");
+        //Util.CreateRecipe(Util.CreateHead("Netherite", CommandLuckyBlock.LB_Texture.get("Netherite")), Arrays.asList("C C", "C C", "C C"), new HashMap<Character, Material>(){{put('C', Material.NETHERITE_INGOT);}}, "Netherite_LB");
+        //Util.CreateRecipe(Util.CreateHead("Redstone", CommandLuckyBlock.LB_Texture.get("Redstone")), Arrays.asList("SSS", "SCS", "SSS"), new HashMap<Character, Material>(){{put('C', Material.REDSTONE_BLOCK); put('S', Material.SCULK);}}, "Redstone_LB");
+        //Util.CreateRecipe(Util.CreateHead("Emerald", CommandLuckyBlock.LB_Texture.get("Emerald")), Arrays.asList("LBL", "EDE", "LBL"), new HashMap<Character, Material>(){{put('L', Material.LAPIS_LAZULI); put('B', Material.LAPIS_BLOCK); put('E', Material.EMERALD_BLOCK); put('D', Material.DIAMOND);}}, "Emerald_LB");
 
         getServer().getPluginManager().registerEvents(new XrayAlerts(), this);
         getServer().getPluginManager().registerEvents(new VillagerAlerts(), this);
