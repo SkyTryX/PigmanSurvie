b5035a7db49f00b9f612eb9df21957f6299b4c6a
SkyTryX <dorian.gil34@gmail.com>
2023-10-10 21:47:09 +0200

    - Ajout de message sur la béta

---
+++ src/main/java/fr/skytryx/pigmansurvie/addons/BetaMSGs.java
@@ -0,0 +1,26 @@
+package fr.skytryx.pigmansurvie.addons;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+
+public class BetaMSGs implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("----------------------------\n"+
+                                         "§bBienvenue sur §6PigmanSMP"+
+                                         "§cCeci est une §c§lBETA, veuillez"+
+                                         "§creport les bugs sur discord!"+
+                                          "----------------------------");
+    }
+
+    @EventHandler
+    public void onStart(PluginEnableEvent event){
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(event.getPlugin(), () -> Bukkit.broadcast(Component.text("[BETA]").color(TextColor.color(255, 0, 0)).append(Component.text(" Le serveur est en béta, veuillez report les bugs sur le discord!").color(TextColor.color(0, 0, 255)))), 120L, 120L);
+    }
+}
